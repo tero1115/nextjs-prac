765a89b36522416316b49d9b7330092a6736c643
tero1115 <limjisang1115@gmail.com>
2023-10-05 14:33:09 +0900

    상세페이지 만들기

---
+++ src/app/table/[idx]/page.js
@@ -0,0 +1,5 @@
+export default function DetailPage({params}) {
+    return(
+        <div>상세 페이지 입니다 : {params.idx}</div>
+    );
+}
---
+++ src/app/table/page.js
@@ -0,0 +1,5 @@
+export default function TablePage() {
+    return(
+        <div>테이블 페이지 입니다</div>
+    );
+}
